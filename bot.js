8a5689ea4bbfff7044ea31f0bdd9c3a1fe297eed
prcemre <57257551+prcemre@users.noreply.github.com>
2019-11-03 12:34:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,36 @@
  */
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+const Discord = require("discord.js");
+const moment = require("moment");
+const client = new Discord.Client();
+const db = require("quick.db");
+require("moment-duration-format");
+
+client.on("message", message => {
+  if (message.content.toLowerCase() === prefix + "profil") {
+    message.channel.sendEmbed(
+      new Discord.RichEmbed()
+        .setDescription(`Avatarınız:`)
+        .setImage(`${message.author.avatarURL} `)
+        .setColor("RANDOM")
+    );
+  }
+});
+
+client.on("guildMemberAdd", member => {
+  console.log("" + member.user + "**VoxDei discorduna hoşgeldin!");
+  console.log(member);
+  member.guild.channels
+    .get("627455803519139851")
+    .send("" + member.user + "**  VoxDei discorduna hoşgeldin!");
+});
+
+client.on("guildMemberRemove", member => {
+  member.guild.channels
+    .get("627455803519139851")
+    .send("" + member.user + "**  Guild discordundan ayrıldı...");
+});
 
 if(!process.send) {
 
