1d3d9e90bf6eb12b2e7b82385749cf9b219b92c4
prcemre <57257551+prcemre@users.noreply.github.com>
2019-11-03 12:29:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,17 @@
-const Discord = require("discord.js");
-const moment = require("moment");
-const client = new Discord.Client();
-const db = require("quick.db");
-require("moment-duration-format");
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
 
-let prefix = "+"; //botun ön eki
-let owner = "294124910203240448"; // sizin id'niz
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
-client.on("ready", guild => {
-  client.user
-    .setActivity("VoxDei", { type: "WATCHING" })
-    .then(presence =>
-      console.log(
-        `Activity set to ${presence.game ? presence.game.name : "none"}`
-      )
-    )
-    .catch(console.error);
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] CLIENT: Şu an ` +
-      client.channels.size +
-      ` adet kanala, ` +
-      client.guilds.size +
-      ` adet sunucuya ve ` +
-      client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() +
-      ` kullanıcıya hizmet veriliyor!`
-  );
-  console.log("Bağlandım!");
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
 });
-
-client.on("message", message => {
-  if (message.content.toLowerCase() === prefix + "profil") {
-    message.channel.sendEmbed(
-      new Discord.RichEmbed()
-        .setDescription(`Avatarınız:`)
-        .setImage(`${message.author.avatarURL} `)
-        .setColor("RANDOM")
-    );
-  }
-});
-
-client.on("guildMemberAdd", member => {
-  console.log("User" + member.user.username + "**VoxDei discorduna hoşgeldin!");
-  console.log(member);
-  member.guild.channels
-    .get("627455803519139851")
-    .send("**" + member.user.username + "**  VoxDei discorduna hoşgeldin!");
-});
-
-client.on("guildMemberRemove", member => {
-  member.guild.channels
-    .get("627455803519139851")
-    .send("**" + member.user.username + "**  Guild discordundan ayrıldı...");
-});
-
-client.login(process.env.TOKEN); //değiştirmeyin.
